1cde99bd5efc97adf6b4b93f5e01d34823519dd3
MoonGhost <yilang0989@outlook.com>
2021-05-18 22:52:51 +0800

    canet

--- models/common.py
+++ models/common.py
@@ -109,7 +109,7 @@ class SpatialAttention(nn.Module):
         max_out, _ = torch.max(x, dim=1, keepdim=True)
         y = torch.cat([avg_out, max_out], dim=1)
         y = self.conv1(y)
-        return x*self.sigmoid(y)
+        return x * self.sigmoid(y)
 
 
 class MetaAconC(nn.Module):
@@ -241,6 +241,65 @@ class BottleneckCSP(nn.Module):
         return self.cv4(self.act(self.bn(torch.cat((y1, y2), dim=1))))
 
 
+class h_sigmoid(nn.Module):
+    def __init__(self, inplace=True):
+        super(h_sigmoid, self).__init__()
+        self.relu = nn.ReLU6(inplace=inplace)
+
+    def forward(self, x):
+        return self.relu(x + 3) / 6
+
+
+class h_swish(nn.Module):
+    def __init__(self, inplace=True):
+        super(h_swish, self).__init__()
+        self.sigmoid = h_sigmoid(inplace=inplace)
+
+    def forward(self, x):
+        return x * self.sigmoid(x)
+
+
+class CoordAtt(nn.Module):
+    def __init__(self, inp, oup, reduction=32):
+        super(CoordAtt, self).__init__()
+        self.pool_h = nn.AdaptiveAvgPool2d((None, 1))
+        self.pool_w = nn.AdaptiveAvgPool2d((1, None))
+
+        mip = max(8, inp // reduction)
+
+        self.conv1 = nn.Conv2d(inp, mip, kernel_size=1, stride=1, padding=0)
+        self.bn1 = nn.BatchNorm2d(mip)
+        self.act = h_swish()
+
+        self.conv_h = nn.Conv2d(mip, oup, kernel_size=1, stride=1, padding=0)
+        self.conv_w = nn.Conv2d(mip, oup, kernel_size=1, stride=1, padding=0)
+
+    def forward(self, x):
+        identity = x
+
+        n, c, h, w = x.size()
+        x_h = self.pool_h(x)
+        # print("x_h.shape",x_h.shape)
+        x_w = self.pool_w(x).permute(0, 1, 3, 2)
+        # print("x_w.shape",x_w.shape)
+
+        y = torch.cat([x_h, x_w], dim=2)
+        y = self.conv1(y)
+        y = self.bn1(y)
+        y = self.act(y)
+
+        x_h, x_w = torch.split(y, [h, w], dim=2)
+        x_w = x_w.permute(0, 1, 3, 2)
+
+        a_h = self.conv_h(x_h).sigmoid()
+        a_w = self.conv_w(x_w).sigmoid()
+        # print("a_h",a_h.shape)
+        # print("a_w",a_w.shape)
+        out = identity * a_w * a_h
+
+        return out
+
+
 class C3(nn.Module):
     # CSP Bottleneck with 3 convolutions
     def __init__(self, c1, c2, n=1, shortcut=True, g=1, e=0.5):  # ch_in, ch_out, number, shortcut, groups, expansion
@@ -253,13 +312,15 @@ class C3(nn.Module):
         # self.m = nn.Sequential(*[CrossConv(c_, c_, 3, 1, g, 1.0, shortcut) for _ in range(n)])
         self.eca = eca_layer(c_)
         self.sa = SpatialAttention()
+        self.attn = CoordAtt(c_, c_)
 
     def forward(self, x):
         # print("before:",x.shape)
         y1 = self.m(self.cv1(x))
         y2 = self.cv2(x)
         # print("after:",x.shape)
-        y1 = self.eca(y1)
+        y1 = self.attn(y1)
+        # y1 = self.eca(y1)
         y1 = self.sa(y1)
         return self.cv3(torch.cat((y1, y2), dim=1))
         # return self.cv3(torch.cat((self.m(self.cv1(x)), self.cv2(x)), dim=1))
