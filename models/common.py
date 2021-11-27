431b0239359197f62901c5c183b33ea05fd345e1
MoonGhost <yilang0989@outlook.com>
2021-11-27 14:14:54 +0800

    3434

--- models/common.py
+++ models/common.py
@@ -141,8 +141,8 @@ class DilatedSpatialAttention(nn.Module):
     def __init__(self):
         super().__init__()
 
-        self.conv1 = nn.Sequential(nn.Conv2d(2, 2, 3, 1, 3, 3), nn.SiLU())
-        self.conv2 = nn.Sequential(nn.Conv2d(2, 2, 3, 1, 3, 3), nn.SiLU())
+        self.conv1 = nn.Sequential(nn.Conv2d(2, 2, 3, 1, 4, 4), nn.SiLU())
+        self.conv2 = nn.Sequential(nn.Conv2d(2, 2, 3, 1, 4, 4), nn.SiLU())
         self.conv3 = Conv(2, 1, 1, 1, act=False)
         self.sigmoid = nn.Sigmoid()
 
