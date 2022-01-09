e7e8fcceac90627874b312e945972add6b2d60bf
MoonGhost <yilang0989@outlook.com>
2022-01-09 20:19:02 +0800

    dcachannelSum

--- models/yolo.py
+++ models/yolo.py
@@ -259,9 +259,7 @@ def parse_model(d, ch):  # model_dict, input_channels(3)
             c2 = ch[f] * args[0] ** 2
         elif m is Expand:
             c2 = ch[f] // args[0] ** 2
-        elif m is Fusion:
-            c2 = ch[f[0]]
-        elif m in [Sum, WcaSum, ChannelWeightedSum, SEChannelWeightedSum]:
+        elif m in [Sum, WcaSum, ChannelWeightedSum, SEChannelWeightedSum, DCAChannelWeightedSum, Fusion]:
             c2 = ch[f[0]]
         else:
             c2 = ch[f]
